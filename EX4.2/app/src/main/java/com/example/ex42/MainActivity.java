c5637b22904711bc71ffd4b17c19cac119214e34
Minh Thu <nguyenthithu191224@gmail.com>
2025-05-09 15:27:27 +0700

    first commit

--- EX4.2/app/src/main/java/com/example/ex42/MainActivity.java
+++ EX4.2/app/src/main/java/com/example/ex42/MainActivity.java
@@ -1,24 +1,57 @@
-package com.example.ex42;
+package com.example.bmi;
 
+import java.text.DecimalFormat;
 import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    Button btnChandoan;
+    EditText editChieucao, editCannang, editBMI, editChandoan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Ánh xạ các thành phần giao diện
+        btnChandoan = findViewById(R.id.btnBMI);
+        editChieucao = findViewById(R.id.edtchieucao);
+        editCannang = findViewById(R.id.edtcannang);
+        editBMI = findViewById(R.id.edtBMI);
+        editChandoan = findViewById(R.id.edtChandoan);
+
+        btnChandoan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Lấy chiều cao và cân nặng
+                double H = Double.parseDouble(editChieucao.getText().toString());
+                double W = Double.parseDouble(editCannang.getText().toString());
+
+                // Tính BMI
+                double BMI = W / Math.pow(H, 2);
+
+                // Chẩn đoán
+                String chandoan;
+                if (BMI < 18) {
+                    chandoan = "Bạn gầy";
+                } else if (BMI < 24.9) {
+                    chandoan = "Bạn bình thường";
+                } else if (BMI < 29.9) {
+                    chandoan = "Bạn béo phì độ 1";
+                } else if (BMI < 34.9) {
+                    chandoan = "Bạn béo phì độ 2";
+                } else {
+                    chandoan = "Bạn béo phì độ 3";
+                }
+
+                // Hiển thị kết quả
+                DecimalFormat dcf = new DecimalFormat("0.0");
+                editBMI.setText(dcf.format(BMI));
+                editChandoan.setText(chandoan);
+            }
         });
     }
 }
--- EX4.2/settings.gradle.kts
+++ EX4.2/settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "EX4.2"
 include(":app")
+ 
